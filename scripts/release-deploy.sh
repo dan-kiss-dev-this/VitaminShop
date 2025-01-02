ffc5289b9daa02e8e0ea3a56d76bbf9ad3254fe8
Frederick Engelhardt <fre@vllc.dev>
2025-01-01 18:15:03 -0800

    Add a release ip and release port and run dotnet which can change the binary host ip

--- scripts/release-deploy.sh
+++ scripts/release-deploy.sh
@@ -5,8 +5,9 @@ rm -rf ./backend/bin
 
 unzip -o $RELEASE_ZIP_FILE -d ./
 
-SCRIPT_TO_RUN="./backend/bin/Release/net8.0/$HOST_ARCHITECTURE/publish/backend.dll"
+# SCRIPT_TO_RUN="./backend/bin/Release/net8.0/$HOST_ARCHITECTURE/publish/backend"
+SCRIPT_TO_RUN="dotnet ./backend/bin/Release/net8.0/linux-x64/publish/backend.dll"
 
 chmod +x $SCRIPT_TO_RUN
 
-eval $SCRIPT_TO_RUN
+eval $SCRIPT_TO_RUN --urls "https://$RELEASE_IP:$RELEASE_PORT/"
--- scripts/release-vars.sh
+++ scripts/release-vars.sh
@@ -2,3 +2,5 @@
 export RELEASE_VERSION="backend-1.0.0"
 export RELEASE_ZIP_FILE="release/$RELEASE_VERSION.zip"
 export HOST_ARCHITECTURE="linux-x64"
+export RELEASE_IP="143.110.152.227"
+export RELEASE_PORT="5000"
