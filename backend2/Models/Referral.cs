071ba73d92c7c84b0316297dfb53516a3962ecdb
Dan Kiss <dan.b.kiss@gmail.com>
2025-01-10 18:29:25 -0800

    Add Referral Model

---
+++ backend2/Models/Referral.cs
@@ -0,0 +1,23 @@
+﻿namespace backend2.Models
+{
+    public class Referral
+    { 
+        public int Id { get; set; }
+        public int UserIdReferrer { get; set; }
+        public string EmailRef { get; set; }
+        public string NameRef { get; set; }
+
+        public int ReferringUserId { get; set; }
+
+
+
+        public Referral(int id, int userIdReferrer, string emailRef, string nameRef)
+        {
+            Id = id;
+            UserIdReferrer = userIdReferrer;
+            EmailRef = emailRef;
+            NameRef = nameRef;
+        }
+
+    }
+}
